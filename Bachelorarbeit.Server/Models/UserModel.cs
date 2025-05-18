da37a88506f7dc7213243435f3c13e16b5f80592
Benjamin <aeplibenjamin@gmail.com>
2025-05-18 07:03:05 +0200

    update UserModel.cs

--- Bachelorarbeit.Server/Models/UserModel.cs
+++ Bachelorarbeit.Server/Models/UserModel.cs
@@ -3,10 +3,13 @@
 public class UserModel
 {
     public Guid Id { get; set; }
+    public string Username { get; set; }
     public string Name { get; set; }
     public string Surname { get; set; }
     public string Email { get; set; }
-    public string PasswordHash { get; set; }
+    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordSalt { get; set; }
+    public byte[] RefreshToken { get; set; }
     public bool IsAdmin { get; set; }
     public DateTimeOffset CreatedAt { get; set; }
     
