2a394b74037e140fcf0ff386a442eb52887d29e0
Benjamin <aeplibenjamin@gmail.com>
2025-05-18 07:02:21 +0200

    add db context

---
+++ Bachelorarbeit.Server/DBContext.cs
@@ -0,0 +1,16 @@
+﻿using Bachelorarbeit.Server.Repository.Entities;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bachelorarbeit.Server;
+
+public class MyDbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    public DbSet<UserEntity> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("DefaultConnection");
+    }
+    
+}
