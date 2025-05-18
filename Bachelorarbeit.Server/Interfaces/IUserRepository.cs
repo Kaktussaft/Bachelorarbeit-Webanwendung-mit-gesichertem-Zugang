90393c56bb85bcd5098173b79d5d7829ea1a5bc7
Benjamin <aeplibenjamin@gmail.com>
2025-05-18 10:11:40 +0200

    Add Interface

---
+++ Bachelorarbeit.Server/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Bachelorarbeit.Server.Models;
+
+namespace Bachelorarbeit.Server.Interfaces;
+
+public interface IUserRepository
+{
+    Task<UserModel> GetByIdAsync(Guid id);
+    Task<IEnumerable<UserModel>> GetAllAsync();
+    Task<UserModel> CreateAsync(UserModel user);
+    Task<UserModel> UpdateAsync(UserModel user);
+    Task<UserModel> DeleteAsync(Guid id);
+    Task<UserModel> GetByUsernameAsync(string username);
+    Task<UserModel> GetByEmailAsync(string email);
+}
