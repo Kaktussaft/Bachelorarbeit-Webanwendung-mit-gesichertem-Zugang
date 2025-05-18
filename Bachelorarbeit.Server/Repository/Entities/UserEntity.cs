edda410604a259e3ae2da56c190ea66e877bec76
Benjamin <aeplibenjamin@gmail.com>
2025-05-18 10:11:54 +0200

    make name/surname nullable

--- Bachelorarbeit.Server/Repository/Entities/UserEntity.cs
+++ Bachelorarbeit.Server/Repository/Entities/UserEntity.cs
@@ -4,8 +4,8 @@ public class UserEntity
 {
     public Guid Id { get; set; }
     public string Username { get; set; }
-    public string Name { get; set; }
-    public string Surname { get; set; }
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
     public string Email { get; set; }
     public byte[] PasswordHash { get; set; }
     public byte[] PasswordSalt { get; set; }
