3178efd9ec3792c21c3e81f5ab521551f70cde73
Benjamin <aeplibenjamin@gmail.com>
2025-05-18 10:11:32 +0200

    Add Automapper

---
+++ Bachelorarbeit.Server/Mappings/UserMapper.cs
@@ -0,0 +1,15 @@
+﻿using Bachelorarbeit.Server.Models;
+using Bachelorarbeit.Server.Repository.Entities;
+using AutoMapper;
+
+namespace Bachelorarbeit.Server.Mappings;
+
+public class UserMapper : Profile
+{
+    public UserMapper()
+    {
+        CreateMap<UserModel, UserEntity>();
+        CreateMap<UserEntity, UserModel>();
+    }
+
+}
