795c050eae611a1be031c7e95e36d452c55c3045
Benjamin <aeplibenjamin@gmail.com>
2025-05-18 07:02:55 +0200

    create LoginController

--- Bachelorarbeit.Server/Controllers/LoginController.cs
+++ Bachelorarbeit.Server/Controllers/LoginController.cs
@@ -1,11 +1,73 @@
 ﻿using System;
+using Bachelorarbeit.Server.Controllers.Requests;
+using Bachelorarbeit.Server.Controllers.Responses;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 
-namespace Bachelorarbeit.Server.Controllers
+namespace Bachelorarbeit.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
 {
+    private readonly IConfiguration _configuration;
 
-    public class LoginController
+    public AuthController(IConfiguration configuration)
     {
+        _configuration = configuration;
+    }
+
+    [HttpPost("login")]
+    public ActionResult<LoginResponse> Login(LoginRequest request)
+    {
+        if (!ValidateUser(request.Email, request.Password))
+        {
+            return BadRequest(new LoginResponse 
+            { 
+                Success = false, 
+                Message = "Invalid credentials" 
+            });
+        }
+
+        var token = GenerateJwtToken(request.Email);
         
+        return Ok(new LoginResponse
+        {
+            Success = true,
+            Token = token,
+            Message = "Login successful"
+        });
+    }
+
+    private string GenerateJwtToken(string email)
+    {
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+            _configuration.GetSection("AppSettings:Token").Value!));
+
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.Email, email)
+        };
+
+        var token = new JwtSecurityToken(
+            claims: claims,
+            expires: DateTime.Now.AddDays(1),
+            signingCredentials: credentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+
+    private bool ValidateUser(string email, string password)
+    {
+        return true;
     }
 }
+
