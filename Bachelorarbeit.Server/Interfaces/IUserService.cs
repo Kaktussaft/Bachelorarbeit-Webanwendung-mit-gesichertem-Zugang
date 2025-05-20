1824f8444d9f92803da28bca528f53cd7abf1e14
Benjamin <aeplibenjamin@gmail.com>
2025-05-20 06:27:06 +0200

    IUserService.cs Interface

---
+++ Bachelorarbeit.Server/Interfaces/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using Bachelorarbeit.Server.Dtos;
+using Bachelorarbeit.Server.Models;
+using Bachelorarbeit.Server.Controllers.Responses;
+
+namespace Bachelorarbeit.Server.Interfaces;
+
+public interface IUserService
+{
+    Task<RegistrationResponse> AddUser(string email, string password, string username);
+    bool VerifyEmailAndPassword(string email, string password);
+    byte[] GetRefreshToken(string email);
+    RefreshTokenDto ReissueRefreshToken(byte[] oldRefreshToken);
+    string FindUsernameByToken(byte[] refreshToken);
+    UserModel FindUserByUsername(string username);
+    Task<bool> UpdateUser(UserModel entity);
+    Task<bool> ResetPasswordAsync(string email, string password);
+    byte[] GeneratePasswordSalt();
+}
