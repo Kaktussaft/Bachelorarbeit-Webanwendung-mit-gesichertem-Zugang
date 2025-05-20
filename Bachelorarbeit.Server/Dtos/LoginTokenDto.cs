4da858ace34ad4895a8a23e6122a993bc5affc7f
Benjamin <aeplibenjamin@gmail.com>
2025-05-20 06:27:27 +0200

    Add LoginTokenDto.cs and RefreshTokenDto.cs

---
+++ Bachelorarbeit.Server/Dtos/LoginTokenDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Bachelorarbeit.Server.Dtos;
+
+public class LoginTokenDto
+{
+    public string AccessToken { get; set; }
+    public string RefreshToken { get; set; }
+    public DateTime Expiration { get; set; } 
+}
---
+++ Bachelorarbeit.Server/Dtos/RefreshTokenDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Bachelorarbeit.Server.Dtos;
+
+public class RefreshTokenDto
+{
+    public byte[] RefreshToken { get; set; }
+    public string Email { get; set; }
+}
