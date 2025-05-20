e6a5dc55a3ef314b21ab3cdd23c1fb9a572b8d0a
Benjamin <aeplibenjamin@gmail.com>
2025-05-20 18:33:40 +0200

    add routes

---
+++ bachelorarbeit.client/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from '../pages/login/login.component';
+import { HomeComponent } from '../pages/home/home.component';
+import { AuthGuard } from '../auth.guard';
+
+
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '' },
+];
