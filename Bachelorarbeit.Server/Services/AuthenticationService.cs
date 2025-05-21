3f58865927b3e87b92d3b9e426264b1c54478cc0
Benjamin <aeplibenjamin@gmail.com>
2025-05-21 18:05:14 +0200

    Add AuthenticationService

---
+++ Bachelorarbeit.Server/Services/AuthenticationService.cs
@@ -0,0 +1,18 @@
+﻿using Bachelorarbeit.Server.Config;
+using Bachelorarbeit.Server.Interfaces;
+
+namespace Bachelorarbeit.Server.Services;
+
+public class AuthenticationService 
+{
+    private readonly IUserService _userService;
+    private readonly JWT_Configuration _jwtConfiguration;
+    public AuthenticationService(IUserService userService, JWT_Configuration jwtConfiguration)
+    {
+        _userService = userService;
+        _jwtConfiguration = jwtConfiguration;
+    }
+    
+    
+    
+}
