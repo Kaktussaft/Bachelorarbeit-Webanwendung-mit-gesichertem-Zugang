895d611bf126f5a42eecc89b6e0e4b9874662708
Benjamin <aeplibenjamin@gmail.com>
2025-05-24 08:18:09 +0200

    Access JWT key through dedicated config

---
+++ Bachelorarbeit.Server/Config/JWT-Configuration.cs
@@ -0,0 +1,10 @@
+﻿namespace Bachelorarbeit.Server.Config;
+
+public class JWT_Configuration
+{
+    public string SecurityKey { get; set; }
+    public string Audience { get; set; }
+    public string Consumer { get; set; }
+    public int ExpirationInMinutes { get; set; }
+    
+}
