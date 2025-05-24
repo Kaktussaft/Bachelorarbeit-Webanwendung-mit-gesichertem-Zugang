633eb90be1398e60e34d53f97c2cd8b8ed5f40e4
Benjamin <aeplibenjamin@gmail.com>
2025-05-24 08:29:11 +0200

    Add RegistrationController.cs

---
+++ Bachelorarbeit.Server/Controllers/RegistrationController.cs
@@ -0,0 +1,32 @@
+﻿using Bachelorarbeit.Server.Controllers.Requests;
+using Bachelorarbeit.Server.Controllers.Responses;
+using Bachelorarbeit.Server.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bachelorarbeit.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RegistrationController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public RegistrationController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [AllowAnonymous]
+    [HttpPost]
+    public IActionResult Register([FromBody] RegistrationRequest request)
+    {
+        var result =   _userService.AddUser(request.userName, request.userMail, request.password).Result;
+
+        if (!result.Success)
+        {
+            return BadRequest(result.Message);
+        }
+        return Ok(result.Message);
+    }
+}
