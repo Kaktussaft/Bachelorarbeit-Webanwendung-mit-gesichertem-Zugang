76d96d88cec76560fe43c23ffa7cbb12e3ac09d4
Benjamin <aeplibenjamin@gmail.com>
2025-05-24 08:18:48 +0200

    refactor LoginController.cs

--- Bachelorarbeit.Server/Controllers/LoginController.cs
+++ Bachelorarbeit.Server/Controllers/LoginController.cs
@@ -1,73 +1,34 @@
 ﻿using System;
 using Bachelorarbeit.Server.Controllers.Requests;
 using Bachelorarbeit.Server.Controllers.Responses;
-using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
+using Bachelorarbeit.Server.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 
 
 namespace Bachelorarbeit.Server.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class AuthController : ControllerBase
+public class LoginController : ControllerBase
 {
     private readonly IConfiguration _configuration;
+    private readonly IAuthenticationService _authenticationService;
 
-    public AuthController(IConfiguration configuration)
+    public LoginController(IConfiguration configuration, IAuthenticationService authenticationService)
     {
         _configuration = configuration;
+        _authenticationService = authenticationService;
     }
 
-    [HttpPost("login")]
-    public ActionResult<LoginResponse> Login(LoginRequest request)
+    [AllowAnonymous]
+    [HttpPost]
+    public IActionResult Login([FromBody] LoginRequest request)
     {
-        if (!ValidateUser(request.Email, request.Password))
-        {
-            return BadRequest(new LoginResponse 
-            { 
-                Success = false, 
-                Message = "Invalid credentials" 
-            });
-        }
+        var loginResult = _authenticationService.Login(request.Email, request.Password);
+        if (!loginResult.IsSuccess)
+            return BadRequest(loginResult.Error);
 
-        var token = GenerateJwtToken(request.Email);
-        
-        return Ok(new LoginResponse
-        {
-            Success = true,
-            Token = token,
-            Message = "Login successful"
-        });
-    }
-
-    private string GenerateJwtToken(string email)
-    {
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
-            _configuration.GetSection("AppSettings:Token").Value!));
-
-        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
-
-        var claims = new[]
-        {
-            new Claim(ClaimTypes.Email, email)
-        };
-
-        var token = new JwtSecurityToken(
-            claims: claims,
-            expires: DateTime.Now.AddDays(1),
-            signingCredentials: credentials
-        );
-
-        return new JwtSecurityTokenHandler().WriteToken(token);
-    }
-
-    private bool ValidateUser(string email, string password)
-    {
-        return true;
+        return Ok(LoginResponse.GetSuccessfullLoginResponse(loginResult.RefreshToken));
     }
 }
-
