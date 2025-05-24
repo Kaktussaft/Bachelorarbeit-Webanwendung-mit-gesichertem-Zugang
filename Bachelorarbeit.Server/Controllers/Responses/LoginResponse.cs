bad64537497d466a10e14d328974eb5195066c73
Benjamin <aeplibenjamin@gmail.com>
2025-05-24 08:28:30 +0200

    Add Loginresponse

--- Bachelorarbeit.Server/Controllers/Responses/LoginResponse.cs
+++ Bachelorarbeit.Server/Controllers/Responses/LoginResponse.cs
@@ -3,8 +3,27 @@
  public class LoginResponse
  {
   public bool Success { get; set; }
-  public string Token { get; set; }
+  public string? Token { get; set; }
   public string? Message { get; set; }
+  
+  public static LoginResponse GetSuccessfullLoginResponse(string refreshToken)
+  {
+   return new LoginResponse()
+   {
+    Message = "Login successful",
+    Success = true,
+    Token = refreshToken
+   };
+  }
+  public static LoginResponse GetFailedLoginResponse()
+  {
+   return new LoginResponse()
+   {
+    Message = "Login failed",
+    Success = false
+   };
+  }
+  
  }   
 }
 
