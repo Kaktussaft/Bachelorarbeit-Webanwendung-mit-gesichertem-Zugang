b51f07320a5b2666a431a709840a8ebbb8a30d41
Benjamin <aeplibenjamin@gmail.com>
2025-05-24 08:17:39 +0200

    add Registration Response

--- Bachelorarbeit.Server/Controllers/Responses/RegistrationResponse.cs
+++ Bachelorarbeit.Server/Controllers/Responses/RegistrationResponse.cs
@@ -1,11 +1,38 @@
 ﻿
 
+using Bachelorarbeit.Server.Dtos;
+
 namespace Bachelorarbeit.Server.Controllers.Responses
 {
     public class RegistrationResponse
     {
         public bool Success { get; set; }
-        public string Token { get; set; }
         public string? Message { get; set; }
+
+        public static RegistrationResponse GetUserNameExistsResponse()
+        {
+            return new RegistrationResponse()
+            {
+                Message = "user already exists",
+                Success = false
+            };
+        }
+        
+        public static RegistrationResponse GetUserMailExistsResponse()
+        {
+            return new RegistrationResponse()
+            {
+                Message = "Email already exists",
+                Success = false
+            };
+        }
+        public static RegistrationResponse GetSuccessfullRegistrationResponse()
+        {
+            return new RegistrationResponse()
+            {
+                Message = "User registered successfully",
+                Success = true,
+            };
+        }
     }
 }
